e33bf5ef887dd4ab809ee59a60cb0ba8ace611dd
Angad Gill <angadsinghgill@gmail.com>
2015-07-26 22:50:08 -0700

    Added PCA. This reduced the error compared the to case where each component was a dimension, however the error is worse than the first attempt.

--- process_train_test.py
+++ process_train_test.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import rmsle
 
+print "Importing data..."
 train = pd.read_csv('data/train_set.csv', parse_dates=[2])
 test = pd.read_csv('data/test_set.csv', parse_dates=[3])
 tube = pd.read_csv('data/tube.csv')
@@ -9,20 +10,32 @@ test2 = pd.merge(test, tube, on='tube_assembly_id')
 
 #specs = pd.read_csv('data/specs.csv')
 
+print "Creating dimensions..."
 # TODO: Create dimension: number of components
-# TODO: Create dimension for each component
-# Adding a dimension for each component
+
+# Process data to create dimensions related to components
 bill_of_materials = pd.read_csv('data/bill_of_materials.csv')
 b = pd.wide_to_long(bill_of_materials, ['component_id_', 'quantity_'], i='tube_assembly_id', j='count')
 b = b.reset_index()
 b = b.drop('count', axis=1)
-b = b.dropna()
-b = b.pivot_table(index='tube_assembly_id', columns='component_id_', values='quantity_')
-b = b.reset_index()
-b = b.fillna(0)
-train2 = pd.merge(train2, b, on='tube_assembly_id', how='left')
+
+# Adding a dimension for number of components
+# b1 = b.drop('component_id_', axis=1)
+# b1 = b1.groupby('tube_assembly_id').sum()
+# b1 = b1.reset_index()
+# b1.columns = ['tube_assembly_id', 'component_quantity']
+# train2 = pd.merge(train2, b1, on='tube_assembly_id', how='left')
+# train2 = train2.fillna(0)
+# test2 = pd.merge(test2, b1, on='tube_assembly_id', how='left')
+# test2 = test2.fillna(0)
+
+# Adding a dimension for each component
+b2 = b.pivot_table(index='tube_assembly_id', columns='component_id_', values='quantity_')
+b2 = b2.reset_index()
+b2 = b2.fillna(0)
+train2 = pd.merge(train2, b2, on='tube_assembly_id', how='left')
 train2 = train2.fillna(0)
-test2 = pd.merge(test2, b, on='tube_assembly_id', how='left')
+test2 = pd.merge(test2, b2, on='tube_assembly_id', how='left')
 test2 = test2.fillna(0)
 
 # TODO: Create dimension using component specs if needed
@@ -57,9 +70,15 @@ X_test = X_test.replace('N', 0)
 y = train2['cost']
 (m, _) = X.shape
 
-from sklearn.linear_model import LinearRegression
+#PCA
+from sklearn.decomposition import PCA
+pca = PCA(n_components=5)
+pca.fit(X,y)
+X = pca.transform(X)
 
+from sklearn.linear_model import LinearRegression
 model = LinearRegression(normalize=True)
+print "Running Linear Regression..."
 model.fit(X[:m/2], y[:m/2])
 score = model.score(X[m/2:], y[m/2:])
 print "Score:", score
@@ -77,7 +96,12 @@ print "Error:", error
 
 
 # Train model on full data and create submission file
+pca = PCA(n_components=5)
+pca.fit(X_test,y)
+X_test = pca.transform(X_test)
+
 model = LinearRegression(normalize=True)
+print "Running Linear Regression on test data..."
 model.fit(X, y)
 output = model.predict(X_test)
 for i,p in enumerate(output):
@@ -86,3 +110,6 @@ for i,p in enumerate(output):
 
 output = pd.DataFrame(output, index=range(1, len(output)+1))
 output.to_csv('output.csv', index=True, header=['cost'], index_label='id')
+
+
+print "Done!"
